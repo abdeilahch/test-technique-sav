928b4c9fe4e442bfad23ffc3cb0a3ba05c1dfb26
abdeilahch <network@a-charai.com>
2025-10-29 23:58:12 +0100

    v1

--- app/core/exceptions.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, status
-
-class TicketNotFound(HTTPException):
-    def __init__(self):
-        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail="Ticket not found")
-
-class InvalidTicketStatus(HTTPException):
-    def __init__(self):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid ticket status")
